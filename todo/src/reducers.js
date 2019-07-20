e18fb531bf52883c1a3104cc4c4e7f568b32a011
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-07-19 20:21:48 -0500

    reducer setup

---
+++ todo/src/reducers.js
@@ -0,0 +1,34 @@
+import {NEWTODO, TOGGLEDONE} from '../actions'
+
+const initialState ={
+        todo:[
+        {
+            newTodo:'',
+            id:1,
+            done: false
+        }
+    ]
+} 
+
+const indexReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case NEWTODO:
+        return {
+          todo: [...state.todo, action.payload]
+        };
+        case TOGGLEDONE:
+        return{
+            todo:state.todo.map(todo =>{
+                if(todo.id !== action.payload.id){
+                    return todo
+                }
+                else{
+                    return action.payload
+                }})}
+
+           default:
+        return state;
+    }
+  };
+
+  export default indexReducer
